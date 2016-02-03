eff9dccb05f96aaef27a3b8cbe9bbc1d4fbffdfd
Beth Gilomen <gilomen2@gmail.com>
2016-02-03 14:55:02 -0600

    trying to fix mail on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'gilomen-timefor.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true  
 end
