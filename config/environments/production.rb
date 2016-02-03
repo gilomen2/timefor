18d17bcb5f7b041f7f5f22fb1dd74fed251f30db
Beth Gilomen <gilomen2@gmail.com>
2016-02-03 17:18:03 -0600

    changing mail sender host to custom domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'gilomen-timefor.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'timefor.io' }
 end
