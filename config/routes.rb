c586e753bfddbd0cbe97d9bc21e404e28fceffa4
Beth Gilomen <gilomen2@gmail.com>
2016-02-04 18:28:45 -0600

    set up model, views, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :contacts
+
   devise_for :users, :controllers => { :registrations => :registrations, :sessions => :sessions, :confirmations => :confirmations, :passwords => :passwords }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
