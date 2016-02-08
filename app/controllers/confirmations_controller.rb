1cfdf3a8a79e7499e7f650b656fcc0150b439481
Beth Gilomen <gilomen2@gmail.com>
2016-02-08 15:54:03 -0600

    fixed to load parsely validation call only on pages with forms

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,11 +1,13 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def new
     @body_class = "am-splash-screen"
+    @includes_form = true
     super
   end
 
   def create
     @body_class = "am-splash-screen"
+    @includes_form = true
     super
   end
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
     @body_class = "am-splash-screen"
+    @includes_form = true
     super
   end
 
   def create
     @body_class = "am-splash-screen"
+    @includes_form = true
     super
   end
 
