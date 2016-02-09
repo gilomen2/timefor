ae1f150a562a62b108afc190bf66886228a1171c
Beth Gilomen <gilomen2@gmail.com>
2016-02-08 21:34:18 -0600

    stub out basis of schedules

---
+++ app/controllers/admin/schedules_controller.rb
@@ -0,0 +1,20 @@
+class Admin::SchedulesController < ApplicationController
+  def index
+    @schedules = Schedule.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
