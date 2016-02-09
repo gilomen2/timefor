fbe70b6ca18ec2054946153c327d95c4d6bf798c
Beth Gilomen <gilomen2@gmail.com>
2016-02-08 23:20:09 -0600

    basic schedule CRUD working. missing frequency and post to call scheduler

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,8 @@
 class Schedule < ActiveRecord::Base
   belongs_to :contact
+  has_one :user
   has_many :scheduled_calls
   has_one :frequency
+
+  delegate :name, :to => :contact, :prefix => true
 end
