20799554df5fef06edd735836b9c696ee1b03254
Beth Gilomen <gilomen2@gmail.com>
2016-02-08 20:34:14 -0600

    fixes #29 clean up app to restrict access to admin area and scope contacts to only a user's own.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/admin', to: redirect('/admin/dashboard')
   # get 'admin/dashboard' => 'dashboard#index'
 
   # Example of regular route:
