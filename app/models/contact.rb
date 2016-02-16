88f3e66c8664f96c58d130958f63db7b8d69f76d
Beth Gilomen <gilomen2@gmail.com>
2016-02-15 19:39:00 -0600

    removed unneeded comment

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,12 +2,4 @@ class Contact < ActiveRecord::Base
   validates_presence_of :name, :phone
 	belongs_to :user
   has_many :schedules
-  # before_save :format_phone
-
-  # def format_phone
-  #   phone = Phonelib.parse(self.phone)
-  #   self.phone = '1'+phone.sanitized
-  # end
-
-
 end
