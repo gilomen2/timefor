c76dd2e33cd6218631103bf3d5fd5af9f571f847
Beth Gilomen <gilomen2@gmail.com>
2016-03-05 18:38:32 -0600

    fixes #36

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,14 @@
 class Contact < ActiveRecord::Base
+  before_destroy :delete_schedules_with_contact
   validates_presence_of :name, :phone
 	belongs_to :user
   has_many :schedules
+
+  private
+
+    def delete_schedules_with_contact
+      schedules_with_contact = Schedule.where(contact: self)
+      schedules_with_contact.destroy_all
+    end
+
 end
