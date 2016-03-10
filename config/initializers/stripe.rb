5093e0719c80c8e689dd1250510eca8dc7dfa002
Beth Gilomen <gilomen2@gmail.com>
2016-03-10 14:11:23 -0600

    remove api keys. keys regenerated for evn variables.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => 'pk_test_Y7GAR7DRvRMttaT9NDgO8x4o',
-    :secret_key      => 'sk_test_b6OoZ7ZZmrTPHMZXpvst6dA9'
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY']
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
-Stripe.api_key = 'sk_test_b6OoZ7ZZmrTPHMZXpvst6dA9'
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
