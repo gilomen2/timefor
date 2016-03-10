10b849559c37c9c4b6da608782b53fd20f03c67c
Beth Gilomen <gilomen2@gmail.com>
2016-03-10 01:23:32 -0600

    working stripe event hooks

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,6 @@
+Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_Y7GAR7DRvRMttaT9NDgO8x4o',
+    :secret_key      => 'sk_test_b6OoZ7ZZmrTPHMZXpvst6dA9'
+}
+
+Stripe.api_key = 'sk_test_b6OoZ7ZZmrTPHMZXpvst6dA9'
