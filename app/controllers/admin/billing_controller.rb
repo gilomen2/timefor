5ab33a982f7fbd9a6e930a880a906bc0b4380bbc
Beth Gilomen <gilomen2@gmail.com>
2016-03-11 00:18:05 -0600

    stub out billing page

--- app/controllers/admin/billing_controller.rb
+++ app/controllers/admin/billing_controller.rb
@@ -1,11 +1,28 @@
 class Admin::BillingController < ApplicationController
   def index
     @user = current_user
-    @subscription = Payola::Subscription.where(owner: @user).where(stripe_status: "active")
+    @subscription = lookup_subscription
     respond_to do |format|
         format.html { }
         format.json { render action: 'index' }
         format.js   { render action: 'index' }
     end
+    @card_expired = check_expiration(@subscription)
+    @active_subscription = determine_active(@subscription)
   end
+
+  def check_expiration(subscription)
+    subscription.card_expiration < Time.now
+  end
+
+  def lookup_subscription
+    if Payola::Subscription.where(owner: @user).where(stripe_status: "active").exists?
+      Payola::Subscription.where(owner: @user).where(stripe_status: "active").first
+    end
+  end
+
+  def determine_active(subscription)
+    subscription.present? && (subscription.stripe_status = "active")
+  end
+
 end
