228e8f7cc1cf99dcb828fdea67678b7df2f34751
Beth Gilomen <gilomen2@gmail.com>
2016-03-18 21:52:11 -0500

    finish up user account and sidebar

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,7 +9,6 @@ class Users::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
     stored_location_for(:user) || admin_dashboard_index_path
-    super
   end
 
 end
