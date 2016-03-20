02bde3581db74ca3ba42a9240fedde8d4da8cb14
Beth Gilomen <gilomen2@gmail.com>
2016-03-19 21:49:40 -0500

    fixes #22 build homepage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,8 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( ring-alt.gif )
 Rails.application.config.assets.precompile += %w( loading-done.png )
-Rails.application.config.assets.precompile += %w( welcome.css )
+Rails.application.config.assets.precompile += %w( welcome.scss )
+Rails.application.config.assets.precompile += %w( preload.scss )
 Rails.application.config.assets.precompile += %w( welcome.js )
 Rails.application.config.assets.precompile += %w( logo-200.png )
+Rails.application.config.assets.precompile += %w( status.gif )
